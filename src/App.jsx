58d665d29cdb8e72e739b723fbd546420563a189
Collin Plank <collin.plank@gmail.com>
2024-11-22 14:56:16 -0600

    ProductsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Header } from "./Header";
-import { PhotosPage } from "./PhotosPage";
+import { ProductsPage } from "./ProductsPage";
 import { Footer } from "./Footer";
 
 axios.defaults.baseURL = "http://localhost:3000";
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Header />
-      <PhotosPage />
+      <ProductsPage />
       <Footer />
     </div>
   );
