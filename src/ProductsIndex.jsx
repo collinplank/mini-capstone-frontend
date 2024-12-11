ce901a20af3e9e7126e31670ff98b32c52fc1de4
Collin Plank <collin.plank@gmail.com>
2024-12-11 14:32:10 -0600

    Fix index page

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -7,7 +7,7 @@ export function ProductsIndex({ products, onShow }) {
           <h2 className="font-bold tex pt-5">{product.name}</h2>
           <img src={product.primary_image_url} className="w-48 h-48 object-cover rounded-md mx-auto mb-4" />
           <p className="text-gray-600 text-sm mb-3 text-pretty">{product.description}</p>
-          <p className="font-semibold text-gray-800 mb-5">Price: {product.price}</p>
+          <p className="font-semibold text-gray-800 mb-5">Price: ${product.price}</p>
           <button
             className="bg-black hover:bg-orange-500 text-white font-semibold py-2 px-6 rounded-lg"
             onClick={() => onShow(product)}
